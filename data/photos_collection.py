9eeb97e184e24d7f68dafcb6dbb4263a13f0aebb
dec4 <dc18@illinois.edu>
2021-04-24 22:09:26 -0500

    adding place photos and collection script

---
+++ data/photos_collection.py
@@ -0,0 +1,49 @@
+""" Collect photos for place data created by poptimes_collection.py
+
+Requirements:
+- Get a Google Maps API Key: https://developers.google.com/maps/documentation/places/web-service/get-api-key
+- To use as intended, first run poptimes_collection.py to create a popular times json file (hard-coded filename for now)
+"""
+import googlemaps
+import json
+import traceback
+
+# You should have a file containing your reddit credentials
+# This file should never be shared /or pushed to git
+credentials_file = "./.secrets/credentials.json"
+with open(credentials_file) as f:
+    params = json.load(f)
+
+# Create google api client
+api_key = params['api_key']
+client = googlemaps.Client(api_key)
+
+# Read place data (created by poptimes_collection.py)
+json_file = "uiuc.json"
+with open(json_file) as f:
+    place_data = json.load(f)
+
+for place in place_data:
+
+    place_id = place["id"]
+    # TODO: how to pick filetype ??
+    img_outfile = "{}/{}.jpg".format(json_file.split(".")[0], place_id)
+
+    try:
+        # Make a Place Details request to get photo reference
+        response = client.place(place_id, fields=["photo"])
+        photo_ref = response["result"]["photos"][0]["photo_reference"]
+
+        # Make a Place Photos request and save actual photo
+        with open(img_outfile, 'w+b') as of:
+            for chunk in client.places_photo(photo_ref, max_height=200):
+                if chunk:
+                    of.write(chunk)
+
+    except KeyboardInterrupt:
+        quit()
+
+    except:
+        print("EXCEPTION ON", place_id)
+        # TODO: get actual exception. currently written as a catch-all 
+        continue
